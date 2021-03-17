9c7e066d7faaa0a27e2321bb5914e308ec90cd4d
Chris <git@gbe0.com>
2021-03-17 15:28:27 +0000

    update example

--- example.py
+++ example.py
@@ -16,5 +16,8 @@ log.error('A message to log at error level') # Always logged (verbosity level 0)
 log.warning('A message to log at warning level') # Verbosity level 1
 log.info('A message to log at info level') # Verbosity level 2
 log.verbose('A message to log at verbose level') # Verbosity level 3
-log.debug('A message to log at verbose level') # Verbosity level 4
+log.debug('A message to log at debug level') # Verbosity level 4
 log.trace('A message to log at trace level') # Verbosity level 5
+
+## Log a fatal error message causing script to exit
+log.fatal('A fatal error')
